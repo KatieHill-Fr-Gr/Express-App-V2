6af812ec281f5c41155709f223f53b3e6ad7e627
Katie Hill <khill.fr.gr@gmail.com>
2025-12-11 13:04:37 +0000

    Error Handler completed

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -18,10 +18,38 @@ const errorHandler = (err, req, res, next) => {
         return res.status(404).json({ message: err.message })
     }
 
-    if (err.name === 'InvalidData' {
+    if (err.name === 'InvalidData') {
         return res.status(err.status).json({ [err.field]: err.message })
-    })
+    }
 
+    if (err.name === 'ValidationError') {
+        const fields = {}
 
+        Object.keys(err.errors).forEach(keyName => {
+            fields[keyName] = err.errors[keyName].properties.message
+        })
+
+        return res.status(400).json(fields)
+    }
+
+    if (err.name === 'MongoServerError' && err.code === 11000) {
+        console.log(Object.entries(err.keyValue)[0])
+        const [keyName, keyValue] = Object.keys(err.keyValue)[0]
+        return res.status(400).json({
+            [keyName]: `${keyName}[0].toUpperCase() + keyName.slice?(1)} "${keyValue}" This email is already taken`
+        })
+    }
+
+    if (err.name === 'Unauthorized') {
+        return res.status(401).json({ message: err.message })
+    }
+
+    if (err.name === 'CastError'&& err.kind === 'ObjectId') {
+        return res.status(400).json({ message: 'Not a valid ObjectId. Please try again.' })
+    }
+
+    return res.status(500).json({ message: 'Internal Server Error' })
 
 }
+
+export default errorHandler
